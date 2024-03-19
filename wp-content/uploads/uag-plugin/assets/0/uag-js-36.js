dc4b8b0f5fad3c4fbcfc390f2ed0101bc8a96509
James Harper <jharp949@gmail.com>
2024-03-18 23:46:22 -0700

    Implement bug fix for issue #123

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-36.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"32c6f961","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"email@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-32c6f961', 36 );
+});
+ });
